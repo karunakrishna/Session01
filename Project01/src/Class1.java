c7d2e55a3f5090ec3696c2ddc5162d9e640a2698
karunakrishna <karuna.krishna@exterro.com>
2018-12-20 16:08:18 +0530

    commit by user01

--- Project01/src/Class1.java
+++ Project01/src/Class1.java
@@ -3,6 +3,7 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("pink!!!");
 		for(int i=0;i<=10;i++){
 			System.out.println(i);
 		}
