70a4287ee01c9cddff45b0cc5a8931fb48f3c637
karunakrishna <karuna.krishna@exterro.com>
2018-12-20 16:06:22 +0530

    commit by user 02

--- Project01/src/Class1.java
+++ Project01/src/Class1.java
@@ -3,8 +3,9 @@ public class Class1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for(int i=0;i<=10;i++){
-			System.out.println(i);
+		String [] p = {"apple"};
+		for(String g :p){
+			System.out.println(p);
 		}
 
 	}
