5673fc9aa8779e2facf9448c1f9a8f65dce1452e
karunakrishna <karuna.krishna@exterro.com>
2018-12-20 15:10:13 +0530

    user01 commit01

---
+++ Sample_demo/src/TestDemo.java
@@ -0,0 +1,12 @@
+
+public class TestDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String [] name = {"apple","mango"};
+		for(int i=0;i<name.length;i++){
+			System.out.println(name[i]);
+		}
+	}
+
+}
